f1b885e95ba19e6e2a784a4d5d40fb164a45ff4b
Bess Sadler <bess@curationexperts.com>
2018-12-07 15:23:46 -0500

    Fix

--- spec/requests/csv_static_files_spec.rb
+++ spec/requests/csv_static_files_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "routes for static files from CSV", :type => :request do
+RSpec.describe "routes for static files from CSV", type: :request do
   it "loads static images from the configured directory" do
     get "/blake_image.jpg"
     expect(response.code).to eq "200"
