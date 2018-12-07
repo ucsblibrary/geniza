c45bf4a65fccd04e5d2bf6e20e6e0d9c574db172
Bess Sadler <bess@curationexperts.com>
2018-12-07 15:18:52 -0500

    Add test for static file serving

---
+++ spec/requests/csv_static_files_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe "routes for static files from CSV", :type => :request do
+  it "loads static images from the configured directory" do
+    get "/blake_image.jpg"
+    expect(response.code).to eq "200"
+  end
+end
